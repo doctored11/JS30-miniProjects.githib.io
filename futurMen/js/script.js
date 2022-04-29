b5fb3b0d134c6602c04187b1dd12adc8a6aaf265
drEd11 <93774837+doctored11@users.noreply.github.com>
2022-04-30 00:53:15 +0300

    add day 13

---
+++ futurMen/js/script.js
@@ -0,0 +1,39 @@
+console.log("1"+2+3+4+5+6);
+
+let peopleStyle = document.querySelector('.imgFramePeople')
+peopleStyle.addEventListener('mouseover',function(){
+  peopleStyle.classList.add('rik');
+  
+
+})
+function debounce(func, wait = 50, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+window.addEventListener('scroll', debounce(checkSlide));
